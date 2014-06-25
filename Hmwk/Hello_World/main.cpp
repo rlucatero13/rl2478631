103c58f755ef0d75e4939eb6372178ef151196f3
rlucatero13 <rlucatero@student.rcc.edu>
2014-06-25 13:04:55 -0700

    Signed-off-by: rlucatero13 <rlucatero@student.rcc.edu>

---
+++ Hmwk/Hello_World/main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.cpp
+ * Author: Rodrigo Lucatero
+ * 
+ * Purpose:  First Program
+ */
+
+//System Level Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv){
+    //Output simple text
+    cout << "Hello World" << endl;
+    //Exit stage right!
+    return 0;
+}
