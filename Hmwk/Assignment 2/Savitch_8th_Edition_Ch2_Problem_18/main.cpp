b787c5533dd224cc9b7fd8e4fc57518412056cab
rlucatero13 <rlucatero@student.rcc.edu>
2014-07-01 13:04:08 -0700

    Signed-off-by: rlucatero13 <rlucatero@student.rcc.edu>

--- Hmwk/Assignment 2/Savitch_8th_Edition_Ch2_Problem_18/main.cpp
+++ Hmwk/Assignment 2/Savitch_8th_Edition_Ch2_Problem_18/main.cpp
@@ -5,14 +5,52 @@
  * Created on July 1, 2014, 12:30 PM
  */
 
-#include <cstdlib>
+//System Libraries
+#include <iostream>
 
 using namespace std;
 
-/*
- * 
- */
+//User Libraries
+
+//Global Constant
+
+//Function Prototype
+
+//Execution Begins Here!
 int main(int argc, char** argv) {
+    //Declare Variable
+    //Inputs
+    unsigned short cBarCal=230; //Number of Calories in type of Candy Bar
+    unsigned short wtL;         //Weight in lbs
+    unsigned short ageY;        //Age in years old
+    unsigned short htI;         //Height in inches
+    char gen;                   //Gender of Male or Female (M/m or F/f)
+    //Outputs
+    float bmr;                  //Basal Metabolic Rate
+    float nCBars;               //Number of Candy Bars
+    
+    //Acquiring Inputs
+    cout << "Calculate how many candy bars a day you can eat." << endl;
+    cout << "What is your gender M/F" << endl;
+    cin >> sex;
+    cout << "What is your weight in lbs?" << endl;
+    cin >> wtL;
+    cout << "What is your height in Inches?" << endl;
+    cin >> htI;
+    cout <<"What is your age in years?" << endl;
+    cin >> ageY;
+    
+    //Calculate the BMR
+    if (sex=='F'||sex=='f'){
+        bmr=655+4.3*wtL+4.7*htI-4.7ageY;
+    }else if (sex=='M'||sex=='m'){
+        bmr=66+6.3*wtL+12.9*htI-6.8ageY;
+    }else{
+        cout<<"Gender Invalid"<<endl;
+    }
+    
+    //Output Results
+    
 
     return 0;
 }
